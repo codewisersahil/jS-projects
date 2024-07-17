fd0c94e73db24a2df90bfe0c3bd042a7cdd99536
Code_wiser_Sahil <89509445+codewisersahil@users.noreply.github.com>
2024-07-17 05:39:18 +0000

    check

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 334433
+let accountName ="sahil"
+var accountPassword = "123"
+accountCity ="pune"
+
+/*
+always prefer to use const and let beacause of scope 
+*/
+console.table([accountId, accountName,accountCity,accountPassword])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+number = 12
+
+String = "sahil"
+
+Boolean = false
+null
+undefined 
+BigInt
+// Symbol = unique
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined 
+
+
+// object 
+Array
+
+
+
