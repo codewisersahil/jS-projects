ebd97be38c9995aa202a67b041dccb47a8ea46c0
codewisersahil <sahilpatle1529@gmail.com>
2024-10-11 23:13:13 +0530

    log

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -7,3 +7,12 @@ console.log('');
 console.log('sahil');
 
 
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
