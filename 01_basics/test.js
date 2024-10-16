41a1c46ae4cfc6dcf1ef15f64e604faa687d04ce
codewisersahil <sahilpatle1529@gmail.com>
2024-10-16 22:16:17 +0530

    .

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -35,6 +35,48 @@ console.log(
 
 );
 
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+
+locationbarlog
+
+
+
+
 
 locationbarlog
 
