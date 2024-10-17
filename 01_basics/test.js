e4a1543cc7e5d3cd766b4c34fa73a8f452fededf
codewisersahil <sahilpatle1529@gmail.com>
2024-10-17 23:05:24 +0530

    log

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -71,6 +71,91 @@ console.log(
 
 );
 
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+
+locationbarlog
+
+
+
+
+
+locationbarlog
+
+
+
+
+
 
 locationbarlog
 
