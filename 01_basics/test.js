b8db959177d3c0fdb1460e950005234f60bd514a
codewisersahil <sahilpatle1529@gmail.com>
2024-10-22 12:32:07 +0530

    .

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -167,3 +167,341 @@ locationbarlog
 
 
 
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+
+locationbarlog
+
+
+
+
+
+locationbarlog
+
+
+
+
+
+
+locationbarlog
+
+
+
+
+
+locationbarlog
+
+
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+
+
+console.log("sahil");
+log
+console.log('as sahil');
+console.log('nan');
+console.log('s');
+console.log('');
+console.log('sahil');
+console.log()
+console.log();
+;
+console.log(
+    console.log()
+    console.log();
+    ;
+    console.log();
+    
+
+    console.log();
+    
+    
+
+);
+console.log(
+
+
+);
+
+
+locationbarlog
+
+
+
+
+
+locationbarlog
+
+
+
+
+
+
+locationbarlog
+
+
+
+
+
+locationbarlog
+
+
+
