87d144d27d4aa1ba1e17702827d7a306fdae93f5
codewisersahil <sahilpatle1529@gmail.com>
2024-10-27 21:32:39 +0530

    log

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -31,7 +31,85 @@ accountCity ="pune"
 
 
 /*
+always prefer to use const and let beacause of scope const accountId = 334433
+let accountName ="sahil"
+var accountPassword = "123"
+accountCity ="pune"
+
+
+/*
 always prefer to use const and let beacause of scope 
+*/
+console.table([accountId, accountName,accountCity,accountPassword])
+/*
+prefer not to use VAR 
+because of issue in block scope and functional scope                                            
+*/const accountId = 334433
+let accountName ="sahil"
+var accountPassword = "123"
+accountCity ="pune"
+
+
+/*
+always prefer to use const and let beacause of scope 
+*/
+console.table([accountId, accountName,accountCity,accountPassword])
+/*
+prefer not to use VAR 
+because of issue in block scope and functional scope                                            
+*/const accountId = 334433
+let accountName ="sahil"
+var accountPassword = "123"
+accountCity ="pune"
+
+
+/*
+always prefer to use const and let beacause of scope 
+*/
+console.table([accountId, accountName,accountCity,accountPassword])
+/*
+prefer not to use VAR 
+because of issue in block scope and functional scope                                            
+*/const accountId = 334433
+let accountName ="sahil"
+var accountPassword = "123"
+accountCity ="pune"
+
+
+/*
+always prefer to use const and let beacause of scope 
+*/
+console.table([accountId, accountName,accountCity,accountPassword])
+/*
+prefer not to use VAR 
+because of issue in block scope and functional scope                                            
+*/const accountId = 334433
+let accountName ="sahil"
+var accountPassword = "123"
+accountCity ="pune"
+
+
+/*
+always prefer to use const and let beacause of scope 
+*/
+console.table([accountId, accountName,accountCity,accountPassword])
+/*
+prefer not to use VAR 
+because of issue in block scope and functional scope                                            
+*/const accountId = 334433
+let accountName ="sahil"
+var accountPassword = "123"
+accountCity ="pune"
+
+
+/*
+always prefer to use const and let beacause of scope 
+*/
+console.table([accountId, accountName,accountCity,accountPassword])
+/*
+prefer not to use VAR 
+because of issue in block scope and functional scope                                            
+*/
 */
 console.table([accountId, accountName,accountCity,accountPassword])
 /*
